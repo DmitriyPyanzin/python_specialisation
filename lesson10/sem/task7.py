5cd043faed52cef368fc6b29c61f96aa952c168e
DmitriyPyanzin <lysy_41@mail.ru>
2025-05-05 23:38:59 +0300

    add add_children

---
+++ lesson10/sem/task7.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def get_name(self):
+        pass
+
+    def get_surname(self):
+        pass
+
+
+class Bird(Animal):
+    def get_name(self):
+        pass
+
+
+b = Bird()
+b.get_name()
+b.get_surname()
